f7995dd8e8d11a484f763071f7e5b3501da366bc
Bernard van der Merwe <bernard.vdm@edplatform.com>
2017-04-03 15:52:16 +0200

    Screwing around with groups

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -19,16 +19,33 @@ namespace SignalRChat
             UpdateUserCount();
             return base.OnDisconnected(stopCalled);
         }
-        public void Send(string name, string message)
+        public void Send(string name, string room, string message)
         {
             // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            Clients.Group(room).addChatMessage(name, message);
         }
         public void GetUserCount(){
             Clients.Client(Context.ConnectionId).updateUserCount(HubHelper.GetCount());
         }
         public void UpdateUserCount(){
             Clients.All.updateUserCount(HubHelper.GetCount());
+        }
+
+        public async Task JoinRoom(string playerName ,string roomName)
+        {
+            Clients.Group(roomName).addChatMessage(playerName, " joined.");
+            await Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRoom(string playerName, string roomName)
+        {
+            Clients.Group(roomName).addChatMessage(playerName, " left.");
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+
+        public void GetUserDetails(string roomName) {
+            //check DB and get users from there
+                        
         }
     }
 }
